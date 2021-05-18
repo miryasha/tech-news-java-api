ee8f2a30bd0bdd6fe6e84c11a5fe0928ceac22b8
cat-coder <cat-coder>
2021-05-18 12:27:41 -0500

    Dependency added

---
+++ src/main/java/com/technews/model/User.java
@@ -0,0 +1,22 @@
+package com.technews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@JsonIgnoreProperities({"hibernateLazyInitializer", "handler"})
+@Table(name = "user")
+
+
+public class User {
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    boolean loggedIn;
+
+    private List<Post> posts;
+    private List<Vote> votes;
+    private List<Comment> comments;
+}
